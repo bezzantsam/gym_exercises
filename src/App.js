08b1c3254b21e01ac7804d1dd9e28d0377416bac
Sam <bezzantsam@gmail.com>
2022-08-06 00:07:52 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material';
 import "./App.css";
+import NavBar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App
-        
-    </div>
+    <Box width = "400px">
+        <NavBar />
+        <Routes>
+            <Route path="/" element= {<Home />} />
+            <Route path="/exercise/:id" element= {<ExerciseDetail/>} />
+        </Routes>
+        <Footer />
+    </Box>
+    
  
   )
 }
