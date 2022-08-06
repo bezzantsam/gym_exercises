f13a939e0c3e54fba734df204497b9a4830bd3b6
Sam <bezzantsam@gmail.com>
2022-08-06 02:03:25 -0700

    styles searchbar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {Box, Button, Typography, Stack, TextField} from '@mui/material';
+
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+      Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField 
+        sx={{
+          input: {fontWeight: '700', border: 'none', borderRadius:'4px'},
+          width: {lg:'1170px', xs: "350px"},
+          background_color: '#fff'
+        }}
+        height="76px" 
+        value="" 
+        onChange={(e) => {}}
+        placeholder="Search Exercises"
+        type="text"
+        />
+      </Box>
+    </Stack>
   )
 }
 
