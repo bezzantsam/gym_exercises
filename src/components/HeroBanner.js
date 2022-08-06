974c8b6ba2ebfde758184e26ff0e5d21dad45dc3
Sam <bezzantsam@gmail.com>
2022-08-06 00:57:43 -0700

    Typography and styling

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Box, Stack, Typography } from '@mui/material'
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+   <Box sx={{ mt:
+   {lg: '212px'}, 
+   ml: {sm: '50px'}
+   }} position="relative" p="20px">
+    <Typography color='#ff2625' fontWeight="600" fontSize="26px">
+        Fitness Club
+    </Typography>
+    <Typography fontWeight={700}
+    sx={{ fontSize: {lg: '44px', xs: '40px'} }}> Sweat, Smile <br /> and Repeat 
+    </Typography>
+    <Typography fontSize='22px' lineHeight='35px'> Check out the most effective exercises </Typography>
+   </Box>
   )
 }
 
