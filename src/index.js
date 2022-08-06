3bb5b892903f1d2e9ace05fb4d09615e1f949cd4
Sam <bezzantsam@gmail.com>
2022-08-05 23:52:34 -0700

    assets, images, css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-const root = React.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-    <App />
+  <App />
 )
