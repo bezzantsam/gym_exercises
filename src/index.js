d0f732da82672d80444ad86b779d90d37a74e6a3
Sam <bezzantsam@gmail.com>
2022-08-05 20:31:28 -0700

    index.js app.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+const root = React.createRoot(document.getElementById('root'));
+
+
+root.render(
+    <App />
+)
