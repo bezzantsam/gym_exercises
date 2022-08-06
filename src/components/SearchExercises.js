53964bf6a2ffb6155bfa5b65fd3c0b9c5550ad90
Sam <bezzantsam@gmail.com>
2022-08-06 03:47:53 -0700

    logic, horizontal scrollbar component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Typography, Stack, TextField} from '@mui/material';
-//41:17
+import {exerciseOptions, fetchData} from '../utils/fetchData'
+
+import HorizontalScrollBar from './HorizontalScrollbar'
+
 
 const SearchExercises = () => {
+
+  const [search, setSearch] = useState('');
+
+  const [exercises, setExercises] =  useState([]);
+  
+  const [bodyParts, setBodyParts] = useState([]);
+
+  
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+   if(search){
+    const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+    console.log(exercisesData);
+
+    const searchedExercises = exercisesData.filter((exercise) => exercise.name.toLowerCase().includes(search)
+    || exercise.target.toLowerCase().includes(search)
+    || exercise.equipment.toLowerCase().includes(search)
+    || exercise.bodyPart.toLowerCase().includes(search)
+    );
+    
+    setSearch('');
+    setExercises(searchedExercises);
+  }
+  }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
@@ -12,15 +49,32 @@ const SearchExercises = () => {
         <TextField 
         sx={{
           input: {fontWeight: '700', border: 'none', borderRadius:'4px'},
-          width: {lg:'1170px', xs: "350px"},
+          width: {lg:'800px', xs: "350px"},
           background_color: '#fff', borderRadius: "40px"
         }}
         height="76px" 
-        value="" 
-        onChange={(e) => {}}
+        value={search} 
+        onChange={(e) => {setSearch(e.target.value.toLowerCase())}}
         placeholder="Search Exercises"
         type="text"
         />
+        <Button className="search-btn"
+        sx={{ 
+          bgcolor: '#ff2625', color: '#fff',
+          textTransform: 'none', width: {lg:'175px', xs: '80px'},
+          fontSize: {lg:'20px', xs:'14px'},
+          height: '56px', 
+          position: 'absolute', 
+          right: '0'
+        }}
+        onClick={handleSearch}
+        >
+          Search
+          </Button>
+      </Box>
+      <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollBar data={bodyParts}/>
+
       </Box>
     </Stack>
   )
---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+
+
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'a1eed637aemsh58e64b984be8593p1b48fcjsn655d3219ab7f',
+    },
+  };
+  
+//   export const youtubeOptions = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+//       'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+//     },
+//   };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
