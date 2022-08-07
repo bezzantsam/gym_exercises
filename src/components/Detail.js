41ae38a511d75b899ff4afd9c7027801d4ca7de1
Sam <bezzantsam@gmail.com>
2022-08-07 06:56:07 -0700

    exercise detail finish

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -27,9 +27,12 @@ const Detail = ({exerciseDetail}) => {
             </Typography>
             {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
-            <Button>
-                <img src={item.icon} />
+            <Button sx={{background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px'}}>
+                <img src={item.icon} alt={bodyPart} style= {{width: '50px', height: '50px'}}/>
             </Button>
+            <Typography textTransform= "capitalize" variant="h5">
+                {item.name}
+            </Typography>
             </Stack>
             ))}
         </Stack>
