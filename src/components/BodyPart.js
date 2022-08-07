a445a19e79dbf7b889c1b97b588c8c871e8be9d0
Sam <bezzantsam@gmail.com>
2022-08-07 02:56:11 -0700

    Typography + styles

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -20,7 +20,8 @@ const BodyPart = ({item, setBodyPart, bodyPart}) => {
     }}
     
     > 
-      <img src={Icon} alt="dumbell" styles= {{width: '40px', height:'40px' }} />
+      <img src={Icon} alt="dumbell" style= {{width: '40px', height:'40px' }} />
+      <Typography fontSize="24px" fontWeight= 'bold' color= '3a1212' textTransform= 'capitalize'>{item}</Typography>
     </Stack>
   )
 }
