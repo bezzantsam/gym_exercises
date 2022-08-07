2e544f8695c4e1bf934ea656b24c458b1f27b191
Sam <bezzantsam@gmail.com>
2022-08-07 04:01:55 -0700

    ExerciseCard finished

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -13,6 +13,9 @@ const ExerciseCard = ({exercise}) => {
             {exercise.target}
         </Button>
        </Stack>
+       <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="1opx" textTransform="capitalize" fontSize="22px">
+            {exercise.name}
+       </Typography>
     </Link> 
   )
 }
