5facd2a2c765eb5e6918e5d181be39a21de24253
Sam <bezzantsam@gmail.com>
2022-08-07 06:49:15 -0700

    Detail button and stack

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,17 +7,31 @@ import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({exerciseDetail}) => {
     const {bodyPart, gifUrl, name,target, equipment} = exerciseDetail;
+
+    const extraDetail = [
+        {icon: BodyPartImage, name: bodyPart},
+        {icon: TargetImage, name: target},
+        {icon: EquipmentImage, name: equipment},
+        
+    ]
   return (
     <Stack gap="60px" sx={{flexDirection: {lg: 'row' }, p: '20px', alignItems: 'center'}}> 
      <img src={gifUrl} alt={name} loading='lazy' className = 'detail-image'/>
         <Stack sx={{gap: {lg: '35px', xs: '20px'}}}>
-            <Typography>
+            <Typography target ="h3">
                 {name}
             </Typography>
-            <Typography>
+            <Typography variant="h6">
                 Exercises keep you strong. {name} {` `}
-                is one of the best exercises to target your abs. It will help you improve your mood and energy.
+                is one of the best exercises to target your {target}. It will help you improve your mood and energy.
             </Typography>
+            {extraDetail?.map((item) => (
+          <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            <Button>
+                <img src={item.icon} />
+            </Button>
+            </Stack>
+            ))}
         </Stack>
     </Stack>
   )
