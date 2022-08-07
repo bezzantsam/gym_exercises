88a4bee0a4e6d98984520b0bc8794b16ac557972
Sam <bezzantsam@gmail.com>
2022-08-07 03:56:47 -0700

    buttons with props

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -5,6 +5,14 @@ const ExerciseCard = ({exercise}) => {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
        <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
+       <Stack direction="row">
+        <Button sx={{ml:'21px', color: '#fff', background: "#ffa9a9", fontSize: '14px', borderRadius: '20px', textTransform:'capitalize'}}>
+            {exercise.bodyPart}
+        </Button>
+        <Button sx={{ml:'21px', color: '#fff', background: "#fcc757", fontSize: '14px', borderRadius: '20px', textTransform:'capitalize'}}>
+            {exercise.target}
+        </Button>
+       </Stack>
     </Link> 
   )
 }
