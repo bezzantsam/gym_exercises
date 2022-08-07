c9849a433f60eb13bca7cd45bafc868a6cde09d6
Sam <bezzantsam@gmail.com>
2022-08-07 06:36:40 -0700

    Detail Typography

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Typography, Stack, Button} from '@mui/material'
 
-const Detail = () => {
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = ({exerciseDetail}) => {
+    const {bodyPart, gifUrl, name,target, equipment} = exerciseDetail;
   return (
-    <div>Detail</div>
+    <Stack gap="60px" sx={{flexDirection: {lg: 'row' }, p: '20px', alignItems: 'center'}}> 
+     <img src={gifUrl} alt={name} loading='lazy' className = 'detail-image'/>
+        <Stack sx={{gap: {lg: '35px', xs: '20px'}}}>
+            <Typography>
+                {name}
+            </Typography>
+            <Typography>
+                Exercises keep you strong. {name} {` `}
+                is one of the best exercises to target your abs. It will help you improve your mood and energy.
+            </Typography>
+        </Stack>
+    </Stack>
   )
 }
 
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -16,7 +16,7 @@ const ExerciseDetail = () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
       const youTubeSearchURL = 'https://youtube-search-and-download.p.rapidapi.com';
 
-      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
     }
     fetchExercisesData();
