226f1e73ca2cf9681113d14d92327928b768920a
Sam <bezzantsam@gmail.com>
2022-08-08 05:40:59 -0700

    exercise videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -13,7 +13,7 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
             flexDirection: {lg: 'row'}, 
             gap: {lg: '10px', xs: '0px'}
         }}>
-            {exerciseVideos?.slice(0,3).map((item, index) => (
+            {exerciseVideos?.slice(0,2).map((item, index) => (
 
                 <a
                 key={index}
@@ -23,6 +23,14 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
                 rel="noopener noreferrer"
                 >
                     <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} />
+                    <Box>
+                    <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                {item.video.channelName}
+              </Typography>
+                    </Box>
                 </a>
             ))}
            
