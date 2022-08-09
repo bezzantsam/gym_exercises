8e1918d848c0b771affe3c982ce301109b4d5320
Sam <bezzantsam@gmail.com>
2022-08-09 04:02:58 -0700

    working mvp

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Stack } from '@mui/material'
 import { InfinitySpin } from 'react-loader-spinner'
+
 const Loader = () => {
   return (
     <Stack direction="row" justifyContent= "center"
